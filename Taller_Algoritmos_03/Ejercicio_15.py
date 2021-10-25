ae8ef53fa837b12125df4171ba35e42491cca114
Angel Stiven Hernández Morales <AHERNAN93227@universidadean.edu.co>
2021-10-24 20:23:10 -0500

    Ejercicios Resueltos

--- Taller_Algoritmos_03/Ejercicio_15.py
+++ Taller_Algoritmos_03/Ejercicio_15.py
@@ -24,19 +24,29 @@ lista0 = [0,1/12,0.5,1,5,10,15,15]
 lista1 = [1/12,0.5,1,5,10,15,100,100]
 lista2 = [12,10,11,11.5,12.6,13,14,12]
 
+J = 0
 for i in range(8):
     if B <= 15:
         if (lista0[i] < B <= lista1[i]) and (C < lista2[i]):
              print("Positivo para anemia\n") # Salida
+        else:
+            J += 1
+    if J == 8:
+        print("Negativo para Anemia\n") # Salida
+
         
     
 if B > 15:
         if D == float(1):
             if (lista0[7] < B < lista1[7]) and (C < lista2[6]):
                 print("Positivo para anemia\n") # Salida
+            else: 
+                print("Negativo para anemia\n") # Salida
         else:
             if (lista0[6] < B < lista1[6]) and (C < lista2[7]):
                 print("Positivo para anemia\n") # Salida
+            else: 
+                print("Negativo para anemia\n") # Salida
             
 
 
