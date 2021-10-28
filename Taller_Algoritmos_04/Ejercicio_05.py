7982c0cf0fc73b36b1e655bb72052476ccbe41ab
Angel Stiven Hernández Morales <AHERNAN93227@universidadean.edu.co>
2021-10-27 21:48:31 -0500

    Ejercicios Resueltos

--- Taller_Algoritmos_04/Ejercicio_05.py
+++ Taller_Algoritmos_04/Ejercicio_05.py
@@ -8,21 +8,24 @@ Sumatoria --> int --> B
 
 """
 # Caja negra
-N = 1
+e = 1
 B = 0
-while B != 1000:
-    B = (((N**2) + 1)/N)
-    if B < 1000: 
-        print(N)
-        N += 1
-    else:
-        N -= 1
-        B = (((N**2) + 1)/N)
+N = 0
+while True:
+    d = ((e**2)+1)/e
+    B = B + d
+    N += 1
+    if B > 1000:
+        d = ((e**2)+1)/e
+        B = B - d
+        N -= 1 
         break
-
+    else: 
+        e += 1
+        print(N)
 # Salidas
-print("\nPara que la sumatoria sea cercana a 1000 \nK tiene que tener valor de:",N)
-print(f"El resultado de la sumatoria es {B}\n")
+print("\nEl numero de terminos necesarios es:",N)
+print("El resultado de la sumatoria cuando N es:",N,"\nEs:",B,"\n")
 
 
          
