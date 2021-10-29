263e553447891349b318dea7bd51b6c84a5ad159
Angel Stiven Hernández Morales <AHERNAN93227@universidadean.edu.co>
2021-10-29 12:52:38 -0500

    Ejercicios Resueltos

--- Taller_Algoritmos_04/Ejercicio_05.py
+++ Taller_Algoritmos_04/Ejercicio_05.py
@@ -3,26 +3,21 @@ Entradas: ----
 
 Salidas: El numero de terminos nesesarios para que la exprexión se acerque a 1000
 
-Terminos --> int --> N
-Sumatoria --> int --> B
+Terminos --> int --> E
+Sumatoria --> int --> d
 
 """
 # Caja negra
 e = 1
-B = 0
-N = 0
 while True:
     d = ((e**2)+1)/e
-    B = B + d
-    N += 1
-    if B > 1000:
+    if d > 1000:
+        e -= 1
         d = ((e**2)+1)/e
-        B = B - d
-        N -= 1 
         break
     else: 
+        print(e)
         e += 1
-        print(N)
 # Salidas
-print("\nEl numero de terminos necesarios es:",N)
-print("El resultado de la sumatoria cuando N es:",N,"\nEs:",B,"\n")   
+print("\nEl numero de terminos necesarios es:",e)
+print("El resultado de la sumatoria es:",d,"\ncuando k es:",e,"\n")
